37e66b7f78faa53ae173c4d0643e6c1e1226f4b5
ISMAEL PEREA <ism_pra_rz@outlook.com>
2024-11-30 01:33:33 -0600

    prueba

---
+++ cast.py
@@ -0,0 +1,15 @@
+numero = "147"
+print (numero)
+print (type (numero))
+
+numero = int(numero)
+print (numero)
+print (type (numero))
+
+numero = float(numero)
+print (numero)
+print (type (numero))
+
+numero = complex(numero)
+print (numero)
+print (type (numero))
---
+++ variables.py
@@ -0,0 +1,26 @@
+# variable entero
+numero = 147
+print (numero)
+print (type (numero))
+
+# variable decimal float
+numero = 147.5
+print (numero)
+print (type (numero))
+
+complejo = 1 + 2j
+print(complejo)
+print (type (complejo))
+
+palabra = "hola"
+print (palabra)
+print (type(palabra))
+
+bandera = True
+print(bandera)
+print (type (bandera))
+
+vacio = None
+print (vacio)
+print (type (vacio)) 
+
